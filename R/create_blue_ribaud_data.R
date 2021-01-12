fe2f4bc30df634c5a715bc37713ee53ab0a04591
Rob Wiederstein <khuon68@gmail.com>
2021-01-11 20:41:00 -0600

    tweak blue_ribald_script

--- R/create_blue_ribaud_data.R
+++ R/create_blue_ribaud_data.R
@@ -1,6 +1,5 @@
 library(rvest)
 library(polite)
-library(janitor)
 library(lubridate)
 library(igraph)
 #scrape
@@ -15,11 +14,22 @@ east.bound$heading <- "east"
 #combine west.bound and east.bound into df
 df <- tibble(rbind(west.bound, east.bound))
 #clean names
-df <- df %>% clean_names()
+df <- df %>% janitor::clean_names()
 #col-1, remove footnote brackets
 df$ship <- gsub("\\[[0-9][0-9]\\]", "", df$ship)
-# omit flag
-df$flag <- NULL
+# add flag
+html <- read_html("https://en.wikipedia.org/wiki/Blue_Riband#Eastbound_record_breakers")
+flags <-
+        html %>%
+        html_nodes(".wikitable img") %>%
+        html_attr("alt")
+df.flags <- tibble(flags = flags, country = "")
+df.flags$country[grep("United Kingdom", df.flags$flags)] <- "GBR"
+df.flags$country[grep("United States", df.flags$flags)] <- "USA"
+df.flags$country[grep("Germany", df.flags$flags)] <- "DEU"
+df.flags$country[grep("France", df.flags$flags)] <- "FRA"
+df.flags$country[grep("Italy", df.flags$flags)] <- "ITA"
+df$flag <- df.flags$country
 #abbreviate
 df$line <- gsub("British & American", "BritAm", df$line)
 # from
@@ -67,7 +77,7 @@ state <- c("Ireland",
            "New Jersey",
            "New York",
            "New York",
-           "New York",
+           "New York, USA",
            "New York",
            "England"
 )
@@ -87,11 +97,12 @@ my.geocodes <- lapply(df.places$address, function(x){
 df.places$lat <- sapply(my.geocodes, "[[", 2)
 df.places$lon <- sapply(my.geocodes, "[[", 1)
 #sanity check --google maps upload
-file <- "./data/rkw/blue-ribald-geocode-places.csv"
+file <- "./data/rkw/blue-riband-geocode-places.csv"
 write.csv(df.places, file = file, row.names = F)
 #merge when node dataframe created
 df <- dplyr::select(df, 
                     ship,
+                    flag,
                     year,
                     line,
                     from,
@@ -102,6 +113,7 @@ df <- dplyr::select(df,
                     speed
                     )
 df$n <- 1
+
 df.1 <-
         df %>%
         group_by(from, to) %>%
@@ -112,16 +124,26 @@ edges <- df.1
 nodes <- tibble(city = unique(c(df$from, df$to)))
 nodes <- merge(nodes, df.places)
 colnames(nodes)[1] <- "id"
-
+#write out "total" df
+file <- "./data/rkw/blue_ribald_total.csv"
+write.csv(df, file = file, row.names = FALSE)
+#write out "edges" df
+file <- "./data/rkw/blue_ribald_edges.csv"
+write.csv(edges, file = file, row.names = FALSE)
+#write out "nodes" df
+file <- "./data/rkw/blue_ribald_nodes.csv"
+write.csv(nodes, file = file, row.names = FALSE)
+#create network
 g <- igraph::graph_from_data_frame(d = edges,
                                    directed = T,
                                    vertices = nodes
 )
+#plot igraph kk
 library(qgraph)
 l <- layout_with_kk(g)
 #l <- layout_with_fr(g)
 l <- norm_coords(l, ymin=-10, ymax=10, xmin=-10, xmax=10)
-jpeg(file = "./imgs/blue_ribald_network_graph.jpeg",
+jpeg(file = "./imgs/blue_riband_network_graph.jpeg",
      width = 7,
      height = 5,
      units = "in",
@@ -140,7 +162,7 @@ igraph::plot.igraph(g,
                     vertex.label.dist = -1.3,
                     margin = c(0, 0, 0, 0),
                     asp = 0,
-                    main = "Blue Riband Destinations",
+                    main = "Blue Riband Ports",
                     sub = "1838 - 1952")
 dev.off()
 file <- "./data/blue_ribald.graphml"
@@ -148,13 +170,14 @@ write_graph(g, file = file, format = "graphml")
 
 
 str(df)
-df.1 <- 
+df.2 <- 
         df %>%
-        gather(key = key, value = value, -ship, -year, -line, -from, -to, -heading)
+        gather(key = key, value = value, 
+               -ship, -flag, -year, -line, -from, -to, -heading)
 
 library(ggplot2)
 library(dplyr)
-mydf <- dplyr::filter(df.1, key == "duration" | key == "knots")
+mydf <- dplyr::filter(df.2, key == "duration" | key == "speed")
 p <- ggplot(mydf, aes(year, value, group = heading, color = heading))
 p <- p + geom_line()
 p <- p + facet_grid(. ~ key)
@@ -162,27 +185,34 @@ p
 
 library(maps)
 library(geosphere)
-map("world")
-xlim <- c(-75, 10)
-ylim <- c(40.0000, 55)
-map("world", col="#f2f2f2", fill=TRUE, bg="white", lwd=0.05, xlim=xlim, ylim=ylim)
+pal <- colorRampPalette(c("#333333", "white", "#1292db"))
+colors <- pal(100)
 
-#from, to , weight and a lat lon
+#map("world")
+xlim <- c(-80, 10)
+ylim <- c(30, 60)
+pdf(file = "./imgs/blue_riband_great_circles.pdf",
+    width = 11,
+    height = 7
+)
+    
+    map("world", 
+    col="#191919",
+    fill=TRUE, 
+    bg="#000000", 
+    lwd=0.05,
+    mar = c(0, 0, 0, 0),
+    xlim=xlim, 
+    ylim=ylim
+    )
+dplyr::arrange(edges, weight)
+maxcnt <- max(edges$weight)
 
-airports <- read.csv("http://datasets.flowingdata.com/tuts/maparcs/airports.csv", header=TRUE) 
-flights <- read.csv("http://datasets.flowingdata.com/tuts/maparcs/flights.csv", header=TRUE, as.is=TRUE)
-
-xlim <- c(-171.738281, -56.601563)
-ylim <- c(12.039321, 71.856229)
-map("world", col="#f2f2f2", fill=TRUE, bg="white", lwd=0.05, xlim=xlim, ylim=ylim)
-fsub <- flights[flights$airline == "AA",]
-
-for (j in 1:length(fsub$airline)) {
-        air1 <- airports[airports$iata == fsub[j,]$airport1,]
-        air2 <- airports[airports$iata == fsub[j,]$airport2,]
-        
-        inter <- gcIntermediate(c(air1[1,]$long, air1[1,]$lat), c(air2[1,]$long, air2[1,]$lat), n=100, addStartEnd=TRUE)
-        
-        lines(inter, col="black", lwd=0.8)
+for (i in 1:nrow(edges)) {
+        port1 <- nodes[nodes$id == edges[i,]$from,]
+        port2 <- nodes[nodes$id == edges[i,]$to,]
+        inter <- gcIntermediate(c(port1[1,]$lon, port1[1,]$lat), c(port2[1,]$lon, port2[1,]$lat), n=100, addStartEnd=TRUE)
+        colindex <- round( (edges[i,]$weight / maxcnt) * length(colors) )
+        lines(inter, col=colors[colindex], lwd=2)
 }
-
+dev.off()
